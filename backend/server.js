ba3b3bdf7df283f514da9d07c542346d65894d0f
MarinaPandaCodes <marinapanda0@gmail.com>
2025-05-09 21:19:15 +0530

    feat:backend part done

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import connectCloudinary from './config/cloudinary.js';
+import connectDB from './config/mongodb.js';
+import adminRouter from './routes/adminRoute.js';
 
+dotenv.config();
 //app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const app = express();
+const port = process.env.PORT || 5173;
+connectDB();
+connectCloudinary();
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json()); 
+app.use(cors());
 
 //api endpoints
-app.use('/api/admin',adminRouter)
-// localhost:4000/api/admin/add-doctor
+app.use('/api/admin', adminRouter);
+// localhost:5173/api/admin/add-doctor
 
-app.get('/',(req,res)=>{
-  res.send('API WORKING ')
-})
+app.get('/', (req, res) => {
+  res.send('API WORKING ');
+});
 
-app.listen(port, ()=> console.log("Server Started",port))
+app.listen(port, () => console.log('Server Started', port));
