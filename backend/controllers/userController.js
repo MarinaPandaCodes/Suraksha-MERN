10d206f2bd821b455ccd982d8979f6e4ca4d9f00
soumadip-dev <soumadipmajila@gmail.com>
2025-05-17 18:58:03 +0530

    Controller to cancel the appointment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -206,11 +206,43 @@ const listAppointments = async (req, res) => {
   }
 };
 
+// API to cancel the appointment
+const cancelAppointment = async (req, res) => {
+  try {
+    const { userId, appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    // verify appointment user
+    if (appointmentData.userId !== userId) {
+      return res.json({ success: false, message: 'Unauthorized action' });
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    // releasing that doctor
+    const { docId, slotDate, slotTime } = appointmentData;
+    const doctorData = await doctorModel.findById(docId);
+
+    let slots_booked = doctorData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime);
+
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+
+    res.json({ success: true, message: 'Appointment cancelled' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   bookAppointment,
+  cancelAppointment,
   getProfile,
+  listAppointments,
   loginUser,
   registerUser,
   updateProfile,
-  listAppointments,
 };
