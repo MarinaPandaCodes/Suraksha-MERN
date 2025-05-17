aa3f053cf86d6e9c8e9837b656cffc35fde5ae73
soumadip-dev <soumadipmajila@gmail.com>
2025-05-17 15:35:50 +0530

    wrote controller for booking apoinment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,38 @@
-import validator from 'validator'
-import bcrypt from "bcrypt"
-import userModel from '../models/userModel.js'
-import jwt from "jsonwebtoken"
-import { v2 as cloudinary } from 'cloudinary'
+import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary';
+import jwt from 'jsonwebtoken';
+import validator from 'validator';
+import doctorModel from '../models/doctorModel.js';
+import userModel from '../models/userModel.js';
 
 //API to register user
-const registerUser =async(req,res)=>{
-    try {
-       const {name, email, password } = req.body
-       // Validate if all required fields are provided
-       if ( !name || !password || !email ) {
-        return res.json({success:false,message:"Missing Details"})
-       } 
-       // Validate the email format
-       if (!validator.isEmail(email)) {
-        return res.json({success:false,message:"enter a valid email"})
-       }
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // Validate if all required fields are provided
+    if (!name || !password || !email) {
+      return res.json({ success: false, message: 'Missing Details' });
+    }
+    // Validate the email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: 'enter a valid email' });
+    }
 
     // Validate the strong password
-       if (password.length < 8) {
-        return res.json({success:false,message:"enter a strong password"})
-       }
+    if (password.length < 8) {
+      return res.json({ success: false, message: 'enter a strong password' });
+    }
 
     //Hashing  user's password
     const salt = await bcrypt.genSalt(10); // Generate a salt
     const hashedPassword = await bcrypt.hash(password, salt); // Hash the password with the salt
 
-
-   // Prepare user data for saving to the database
+    // Prepare user data for saving to the database
     const userData = {
       name,
       email: email.toLowerCase(),
       password: hashedPassword,
-    }
+    };
 
     // Create a new user document and save it to the database
     const newUser = new userModel(userData);
@@ -45,7 +45,7 @@ const registerUser =async(req,res)=>{
     console.log(error);
     res.json({ success: false, message: error.message });
   }
-}
+};
 
 // API to user login
 const loginUser = async (req, res) => {
@@ -56,7 +56,7 @@ const loginUser = async (req, res) => {
 
     if (!user) {
       // If the user does not exist, return an error
-      return res.json({ success: false, message: "User does not exist" });
+      return res.json({ success: false, message: 'User does not exist' });
     }
 
     // Compare the provided password with the stored hashed password
@@ -67,7 +67,7 @@ const loginUser = async (req, res) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       res.json({ success: true, token });
     } else {
-      res.json({ success: false, message: "Invalid Credentials" });
+      res.json({ success: false, message: 'Invalid Credentials' });
     }
   } catch (error) {
     console.log(error);
@@ -75,16 +75,13 @@ const loginUser = async (req, res) => {
   }
 };
 
-
-
-
 // API to get User Profile Data
 const getProfile = async (req, res) => {
   try {
     const { userId } = req.body;
 
     // Find the user by ID and exclude the password field
-    const userData = await userModel.findById(userId).select("-password");
+    const userData = await userModel.findById(userId).select('-password');
 
     // Respond with the user data
     res.json({ success: true, userData });
@@ -93,13 +90,6 @@ const getProfile = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
-
-
-
-
-
-
-
 
 // API to update User Profile
 const updateProfile = async (req, res) => {
@@ -110,7 +100,7 @@ const updateProfile = async (req, res) => {
     const imageFile = req.file;
 
     if (!name || !dob || !gender || !phone) {
-      return res.json({ success: false, message: "Data Missing" });
+      return res.json({ success: false, message: 'Data Missing' });
     }
 
     // Update the user's profile details in the database
@@ -125,7 +115,7 @@ const updateProfile = async (req, res) => {
     if (imageFile) {
       // If an image is uploaded, upload it to Cloudinary
       const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
-        resource_type: "image",
+        resource_type: 'image',
       });
 
       // Get the secure URL of the uploaded image
@@ -134,11 +124,73 @@ const updateProfile = async (req, res) => {
       // Update the user's profile with the new image URL
       await userModel.findByIdAndUpdate(userId, { image: imageUrl });
     }
-    return res.json({ success: true, message: "Profile Updated" });
+    return res.json({ success: true, message: 'Profile Updated' });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-export {registerUser, loginUser, getProfile, updateProfile}
+// API to book appointment
+const bookAppointment = async (req, res) => {
+  try {
+    // Extract data from the request body
+    const { userId, docId, slotTime, slotDate } = req.body;
+
+    // Fetch the doctor's data from the database (excluding password)
+    const docData = await doctorModel.findById(docId).select('-password');
+
+    // Check if the doctor exists
+    if (!docData) {
+      return res.json({ success: false, message: 'Doctor not available' });
+    }
+
+    // Get the already booked slots for the doctor
+    let slots_booked = docData.slots_booked;
+
+    // checking for slot availability
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({ success: false, message: 'Slots not available' });
+      } else {
+        // If the time slot is available, add it to the booked slots
+        slots_booked[slotDate].push(slotTime);
+      }
+    } else {
+      // If no slots exist for the given date, create a new array and add the slot
+      slots_booked[slotDate] = [];
+      slots_booked[slotDate].push(slotTime);
+    }
+
+    // Fetch the user's data from the database (excluding password)
+    const userData = await userModel.findById(userId).select('-password');
+
+    // Remove the slots_booked field from the doctor's data before saving it to the appointment
+    //  console.log("slots booked ", docData.slots_booked)
+    delete docData.slots_booked;
+
+    // Create appointment data
+    const appointmentData = {
+      userId,
+      docId,
+      slotTime,
+      slotDate,
+      userData,
+      docData,
+      date: Date.now(),
+      amount: docData.fees,
+    };
+
+    const newAppointment = new appointmentModel(appointmentData);
+    await newAppointment.save(); // Save the new appointment data in the database
+
+    // save new slots data in docData / Update the doctor's booked slots in the database
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+    return res.json({ success: true, message: 'Appointment Booked' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { bookAppointment, getProfile, loginUser, registerUser, updateProfile };
